28df97f0acb0397f0f3cce421679c9a22c97bccc
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-19 09:47:35 +0900

    banners and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'list#index'
   resources :list, only: [:index, :show, :new, :create] do
     resources :bookmark, only: [:new, :create, :destroy]
   end
