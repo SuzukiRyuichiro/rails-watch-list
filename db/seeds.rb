98b324a4b944244800dd107f7bd63416d1118710
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-20 23:46:22 +0900

    made some update

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,6 @@ List.destroy_all
 Movie.destroy_all
 Bookmark.destroy_all
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-
-5.times do
-  book = Bookmark.new(comment: 'Test fucking test', list: List.all.sample, movie: Movie.all.sample)
-  p book if book.save
-end
-
 lists = [
   { api_id: 28, name: 'Action', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f4a3.png' },
   { api_id: 12, name: 'Adventure', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/2708.png' },
@@ -36,20 +26,31 @@ lists = [
 ]
 
 lists.each do |list_info|
-  list = List.create!(name: list_info[:name], api_id: list_info[:api_id])
+  list = List.create!(name: list_info[:name])
   puts "Getting photo for #{list_info[:name]}..."
   file = URI.open(list_info[:url])
   list.photo.attach(io: file, filename: 'emoji.png', content_type: 'image/png')
 end
 
-url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API']}"
-response = JSON.parse(URI.open(url).read)
+(1..30).to_a.each do |digit|
+  url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['MOVIE_API']}&page=#{digit}"
+  response = JSON.parse(URI.open(url).read, { :symbolize_names => true })
 
-response['results'].each do |movie_hash|
-  puts
-  p movie_hash
-  # create an instance with the hash
-  Movie.create!(
-    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path']
-  )
+  response[:results].each do |movie_hash|
+    # create an instance with the hash
+    movie = Movie.new(
+      poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash[:poster_path],
+      title: movie_hash[:title],
+      overview: movie_hash[:overview],
+      rating: movie_hash[:vote_average]
+    )
+    if movie.save
+      puts "#{movie} saved"
+    end
+  end
+end
+
+30.times do
+  book = Bookmark.new(comment: 'Test fucking test', list: List.all.sample, movie: Movie.all.sample)
+  p book if book.save
 end
